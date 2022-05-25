4fff31f530f279b1fef8d1c6555de3f00baa7b4a
Abdul-Rakib92 <rakibctgu@gmail.com>
2022-05-25 21:29:57 +0600

    product delete api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,87 +9,94 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.69otv.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
+async function run() {
+  try {
+    await client.connect();
+    console.log("db connected");
+    const toolCollection = client.db("repairBd").collection("tool");
+    const orderCollection = client.db("repairBd").collection("order");
 
-    try{
-        await client.connect();
-        console.log('db connected');
-        const toolCollection = client.db('repairBd').collection('tool');
-        const orderCollection = client.db('repairBd').collection('order')
+    // tool api
+    app.get("/tool", async (req, res) => {
+      const query = {};
+      const cursor = toolCollection.find(query);
+      const tools = await cursor.toArray();
+      res.send(tools);
+    });
 
-        // tool api
-        app.get('/tool', async(req, res) =>{
-            const query = {};
-            const cursor = toolCollection.find(query);
-            const tools = await cursor.toArray();
-            res.send(tools);
-        })
+    app.get("/tool/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const tool = await toolCollection.findOne(query);
+      res.send(tool);
+    });
 
-        app.get('/tool/:id', async(req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const tool = await toolCollection.findOne(query);
-            res.send(tool);
-        });
+    //Post (add a new user)
+    app.post("/tool", async (req, res) => {
+      const newTool = req.body;
+      const result = await toolCollection.insertOne(newTool);
+      res.send(result);
+    });
 
-        // Order Collection API
+    // Delete tool
+    app.delete('/tool/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await toolCollection.deleteOne(query);
+        res.send(result);
+  
+      })
 
-        // app.get('/order', async(req, res) =>{
-        //     const email = req.query.email;
-        //     // console.log(email);
-        //     const query = {email: email };
-        //     const cursor = orderCollection.find(query);
-        //     const orders = await cursor.toArray();
-        //     res.send(orders);
-        // })
+    // Order Collection API
 
-        app.get('/order', async(req, res) =>{
-            // const email = req.query.email;
-            const query = {};
-            const cursor = orderCollection.find(query);
-            const orders = await cursor.toArray();
-            res.send(orders);
-        })
+    // app.get('/order', async(req, res) =>{
+    //     const email = req.query.email;
+    //     // console.log(email);
+    //     const query = {email: email };
+    //     const cursor = orderCollection.find(query);
+    //     const orders = await cursor.toArray();
+    //     res.send(orders);
+    // })
 
-        app.post('/order', async (req, res) => {
-            const order = req.body;
-            const result = await orderCollection.insertOne(order);
-            res.send(result);
-        })
+    app.get("/order", async (req, res) => {
+      // const email = req.query.email;
+      const query = {};
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
 
-        // Delete order
-        app.delete('/order/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = {_id: ObjectId(id)};
-            const result = await orderCollection.deleteOne(query);
-            res.send(result);
-      
-          })
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
 
-
-
-    }
-
-    finally{
-
-    }
-
+    // Delete order
+    app.delete("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Repair.BD is running");
+});
 
-
-app.get('/', (req, res) => {
-    res.send('Repair.BD is running')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Repair.BD App listening on port ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`Repair.BD App listening on port ${port}`);
+});
